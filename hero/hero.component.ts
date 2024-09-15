0679c7dae5cfcbbf8199c4425b63c427c20e9c17
Hirle <87393381+hirleog@users.noreply.github.com>
2024-09-15 19:46:21 -0300

    responsive layout added: header, hero, service & about

--- hero/hero.component.ts
+++ hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+  @ViewChild('bannerVideo') bannerVideo!: ElementRef;
+  isVideoPlaying: boolean = true;  // Flag para controlar se o vídeo está sendo exibido
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const video: HTMLVideoElement = this.bannerVideo.nativeElement;
+
+    // Tenta carregar o vídeo
+    video.load();
+
+    // Tenta reproduzir o vídeo quando estiver pronto
+    video.addEventListener('canplay', () => {
+      video.play().catch(error => {
+        console.error("Erro ao tentar reproduzir o vídeo, substituindo por imagem:", error);
+        this.isVideoPlaying = false;  // Se der erro, substitui o vídeo pela imagem
+      });
+    });
+
+    // Caso o vídeo falhe ao carregar
+    video.addEventListener('error', () => {
+      console.error("Erro ao carregar o vídeo, substituindo por imagem.");
+      this.isVideoPlaying = false;
+    });
   }
 
 }
